557ce4d8694a228eac763e20e9f19c835ec28d5a
Benjamin Yoo <byoo0522@gmail.com>
2020-08-26 21:42:35 -0700

    2nd commit
    added color to "this"

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -2,8 +2,7 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function () {
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
+        this.style.color = "white";
     });
 }
 
