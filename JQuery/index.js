dac970125175bf98e6cbae36c5ff10d7c8736a36
Benjamin Yoo <byoo0522@gmail.com>
2020-08-31 20:20:11 -0700

    2nd commit
    Playing around with JQuery. incoporating different features. Everything has been commented out and can be activated by uncommenting.

--- JQuery/index.js
+++ JQuery/index.js
@@ -4,6 +4,37 @@
 // });
 
 
-$("h1").addClass("big-title margin-50");
+// $("h1").text("Bye");
 
-$("button");
+// // $("button").html("<em>Hey</em>");
+
+// // console.log($("img").attr("src"));
+
+// $("a").attr("href", "https://www.yahoo.com");
+
+
+// $("h1").click(function () {
+//     $("h1").css("color", "purple");
+// });
+
+
+// $("button").click(function () {
+//     $("h1").css("color", "purple");
+// });
+
+// $("input").keypress(function (event) {
+//     console.log(event.key);
+// });
+
+// $(document).keypress(function (event) {
+//     $("h1").text(event.key);
+// });
+
+// $("h1").on("mouseover", function () {
+//     $("h1").css("color", "purple");
+// });
+
+
+$("button").on("click", function () {
+    $("h1").slideUp().slideDown().animate({ opacity: 0.5 });
+});
