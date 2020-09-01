f6f4443657bf3a567612ed8d01318b4340c05d63
Benjamin Yoo <byoo0522@gmail.com>
2020-08-31 19:22:01 -0700

    1st commit
    Adding h1 via JQuery

---
+++ JQuery/index.js
@@ -0,0 +1,9 @@
+// This code below is if you want to add the script source in title. Adding script at the end before body is better.
+// $(document).ready(function () {
+//     $("h1").css("color", "red");
+// });
+
+
+$("h1").addClass("big-title margin-50");
+
+$("button");
