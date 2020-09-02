3d98bf387f97fd5cf654bbdc1917ae67199b0c3a
Benjamin Yoo <byoo0522@gmail.com>
2020-09-01 20:32:58 -0700

    2nd commit
    created random color/number generator. Need to add flash on click and sound on click.

--- Simon Says/game.js
+++ Simon Says/game.js
@@ -1,1 +1,8 @@
-alert("hello")
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+}
