b3c79bd78e4e5b104267fee9c1152970c3df3737
Benjamin Yoo <byoo0522@gmail.com>
2020-09-22 19:43:10 -0700

    comm

--- My-Express-Server/server.js
+++ My-Express-Server/server.js
@@ -3,4 +3,22 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000);
+app.get("/", function (req, res) {
+    res.send("<h1>Hello, World!</h1>");
+})
+
+app.get("/contact", function (req, res) {
+    res.send("Contact me at Byoo0522@gmail.com!");
+});
+
+app.get("/about", function (req, res) {
+    res.send("My name is Ben and I'm a Web Developer.");
+});
+
+app.get("/hobbies", function (req, res) {
+    res.send("Gaming, Coding, Golf, Beer");
+});
+
+app.listen(3000, function () {
+    console.log("Server has started on port 3000");
+});
