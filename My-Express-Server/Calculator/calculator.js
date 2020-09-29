4fbf0b2c0575cbc6635bff68fd6a8b31f62c02ea
Benjamin Yoo <byoo0522@gmail.com>
2020-09-28 20:28:00 -0700

    BMI Calculator Complete
    Finished BMI calculator and linked to calculator.

--- My-Express-Server/Calculator/calculator.js
+++ My-Express-Server/Calculator/calculator.js
@@ -17,7 +17,18 @@ app.post("/", function (req, res) {
     res.send("The result of the calculation is " + result);
 });
 
+app.get("/bmicalculator", function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
 
+app.post("/bmiCalculator", function (req, res) {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height);
+
+    res.send("Your BMI is " + bmi);
+})
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000.");
