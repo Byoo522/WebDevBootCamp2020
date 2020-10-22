7ff03e3f374cbdf6895b161d8bcb22beffe7baf0
Benjamin Yoo <byoo0522@gmail.com>
2020-10-21 20:15:41 -0700

    3rd commit

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
 app.get("/", function (req, res) {
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Los Angeles&appid=3c16fe54dae4f2afaf5008bc751d883e&units=imperial"
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data)
+            console.log(weatherData);
+            const temp = weatherData.main.temp
+            console.log(temp);
+            const weatherDescription = weatherData.weather[0].description
+            console.log(weatherDescription);
+        })
+    })
     res.send("Server is up and running.")
 })
 
